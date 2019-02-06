2cdcf1010081f4049209f257938fdab5fb38beea
huguettevoicu <huguette.voicu@gmail.com>
2019-02-06 12:44:31 +0200

    import

---
+++ src/Data.java
@@ -0,0 +1,23 @@
+public class Data {
+    public static void main(String[] args) {
+        int myAge;
+        float weight = (float) 60.5;
+        double height = 356.6;
+        long distance = 1000000045674l;
+        char gender = 'm';
+        char test = 101;
+        boolean isStudent = true;
+        boolean hasJob = true;
+        myAge = 31;
+        System.out.println(myAge);
+        System.out.println(weight);
+        System.out.println(height);
+        System.out.println(distance);
+        System.out.println(gender);
+        System.out.println(test);
+
+
+
+
+    }
+}
